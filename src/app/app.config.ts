331069bb087e3c8ad616a1b3edcb39292b53c94a
Nicolas Bruno <nicolsbruno@gmail.com>
2025-02-25 23:33:08 -0300

    feat(app): se agrega configuración para hash strategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
               provideRouter(routes),
-              provideAnimationsAsync()]
+              provideAnimationsAsync(),
+
+              {
+                provide: LocationStrategy,
+                useClass: HashLocationStrategy
+              }
+            ]
 };
